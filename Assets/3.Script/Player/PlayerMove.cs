893344449a51b872af7644f930db348298042b8e
전홍현 <toinbee3@naver.com>
2024-08-27 01:36:50 +0900

    [Feat] Player가 Collider 뚫고 지나가는 이슈 방지
    RayCast 활용. Slope 경사 비정상적으로 오르는 이슈도 방지

--- Assets/3.Script/Player/PlayerMove.cs
+++ Assets/3.Script/Player/PlayerMove.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.UI;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class PlayerMove : MonoBehaviour {
     private PlayerInputActions playerInputAction;
@@ -25,6 +27,8 @@ public class PlayerMove : MonoBehaviour {
     private float rotateSpeed = 8f;
     private float jumpSpeed = 250f;
 
+    private Vector3 pastFramePosition;
+    private Vector3 currentFramePosition;
     private void Awake() {
         playerInputAction = new PlayerInputActions();
         playerCollider = GetComponentInChildren<Collider>();
@@ -34,6 +38,8 @@ public class PlayerMove : MonoBehaviour {
         playerInputAction.PlayerActions.Move.performed += value => OnMove(value.ReadValue<Vector2>());
         playerInputAction.PlayerActions.Dash.performed += value => OnDash();
         playerInputAction.PlayerActions.Jump.performed += value => OnJump();
+
+        pastFramePosition = currentFramePosition = transform.position;
     }
 
     private void OnEnable() {
@@ -47,7 +53,9 @@ public class PlayerMove : MonoBehaviour {
     private void FixedUpdate() {
         Move();
         Rotate();
+
         CheckJumpValidity();
+        CheckWallThroughBack();
     }
 
     private void OnMove(Vector2 value) {
@@ -94,8 +102,13 @@ public class PlayerMove : MonoBehaviour {
         playerRigid.AddForce(Vector3.up * Mathf.Sqrt(jumpSpeed), ForceMode.Impulse);
     }
 
-    private float checkJumpTolerance = 0.1f;
     private void OnCollisionEnter(Collision collision) {
+        CheckLandGround(collision);
+        CheckWallThroughFront();
+    }
+
+    private float checkJumpTolerance = 0.1f;
+    private void CheckLandGround(Collision collision) {
         float colliderTop = collision.collider.bounds.center.y + (collision.collider.bounds.size.y / 2);
         float playerBottom = playerCollider.bounds.center.y - (playerCollider.bounds.size.y / 2);
 
@@ -108,6 +121,37 @@ public class PlayerMove : MonoBehaviour {
             isJump = true;
     }
 
+    private void OnCollisionStay(Collision collision) {
+        if (!(collision.gameObject.layer == LayerMask.NameToLayer("Ground"))) {
+            CheckWallThroughFront();
+            CheckSlopeValidity();
+        }
+    }
+
+    private void CheckWallThroughFront() {
+        Vector3 rayOrigin = transform.position;
+        rayOrigin.y += 2f;
+
+        if (Physics.Raycast(rayOrigin, transform.forward, out RaycastHit rayHit, 1f)) {
+            Debug.Log($"Collider {rayHit.collider.name} is in front of player");
+            currentSpeed = 0;
+        }
+    }
+
+    private void CheckSlopeValidity() {
+        if (isJump) return;
+        Vector3 rayOrigin = transform.position;
+        rayOrigin += transform.forward * 2f;
+
+        if (Physics.Raycast(rayOrigin, Vector3.down, out RaycastHit rayHit, 2f)) {
+            if (Vector3.Angle(Vector3.up, rayHit.normal) > 40f) {
+                Debug.Log("Slope is Collided");
+                currentSpeed = 0f;
+            }
+        }
+
+    }
+
     private Vector3 checkRayBoxSize = new Vector3(2f, 0.03f, 2f);
     private void CheckJumpValidity() {
         playerAnimator.SetBool("isJump", isJump);
@@ -115,11 +159,25 @@ public class PlayerMove : MonoBehaviour {
             Vector3 rayStartPosition = transform.position;
             rayStartPosition.y += 0.6f;
 
-            if (Physics.BoxCast(rayStartPosition, checkRayBoxSize, Vector3.down, transform.rotation, 0.7f)) 
+            if (Physics.BoxCast(rayStartPosition, checkRayBoxSize, Vector3.down, transform.rotation, 0.7f))
                 isJump = false;
-            
         }
     }
+
+
+    private void CheckWallThroughBack() {
+        currentFramePosition = transform.position;
+        currentFramePosition.y += 1f;
+        Vector3 direction = currentFramePosition - pastFramePosition;
+        float distance = direction.magnitude;
+
+        if (Physics.Raycast(pastFramePosition, direction, out RaycastHit rayHit, distance)) {
+            currentSpeed = 0;
+            Debug.Log($"Collider {rayHit.collider.name} is collided at nextFrame");
+        }
+        pastFramePosition = currentFramePosition;
+    }
+
     private void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, checkRayBoxSize);
     }
