fcacd6d4e8c16e5c6c1a915820b806f83a20abf3
전홍현 <toinbee3@naver.com>
2024-09-19 20:12:02 +0900

    [Feat] WordData Property Editor

--- Assets/3.Script/UI/Game/Combine/CombineManager.cs
+++ Assets/3.Script/UI/Game/Combine/CombineManager.cs
@@ -102,8 +102,8 @@ public class CombineManager :MonoBehaviour {
     }
 
     public void SetSlotWords(int index, Word word) {
-        selectedFrame.SetWord(index, word);
-        combineSlotControllers[index].SetSlotWord(word);
+       // selectedFrame.SetWord(index, word);
+       // combineSlotControllers[index].SetSlotWord(word);
     }
 
     //public RectTransform GetSlotRectTransform(int num) {
@@ -138,27 +138,26 @@ public class CombineManager :MonoBehaviour {
             //sentencesManager.SetSlotSentence(selectKey, selectedFrame);
             //selectKey = -1; //TODO: ESC로 돌아올 수 있으므로 selectKey가 저장되어야 할 필요 있음
 
-        if(selectedFrame.CheckSentenceValidity()) {
-            sentencesManager.SetSlotSentence(selectKey, selectedFrame);
-            selectKey = -1; //TODO: ESC�� ���ƿ� �� �����Ƿ� selectKey�� ����Ǿ�� �� �ʿ� ����
-            for (int i = 0; i < selectedFrame.BlankCount; i++)
-                SetSlotWords(i, null);
+            if (baseFrame.CheckSentenceValidity()) {
+                //sentencesManager.SetSlotSentence(selectKey, selectedFrame);
+                //selectKey = -1; //TODO: ESC�� ���ƿ� �� �����Ƿ� selectKey�� ����Ǿ�� �� �ʿ� ����
+                //for (int i = 0; i < selectedFrame.BlankCount; i++)
+                //    SetSlotWords(i, null);
+                //
+                //selectControl.SetTargetTag(selectedFrame.wordA.Tag);
+                CameraControl.Instance.SetCamera(CameraControl.CameraStatus.SelectView);
+                combineContainer.CloseCombineField();
+                halfInvenContainer.CloseCombineInven();
 
-            //TODO: ���ô�� �������� ��� SetTargetTag ���� �ʿ�
-            selectControl.SetTargetTag(selectedFrame.wordA.Tag);
-            CameraControl.Instance.SetCamera(CameraControl.CameraStatus.SelectView);
-            combineContainer.CloseCombineField();
-            halfInvenContainer.CloseCombineInven();
+                //TODO: 확정하더라도 선택해서 사용하는 경우에는 선택한 후에 소모해야함
+            }
+            else
+                dialogContents = "문장을 조합 할 수 없습니다\n단어를 확인해주세요.";
 
-            //TODO: 확정하더라도 선택해서 사용하는 경우에는 선택한 후에 소모해야함
+            if (dialogContents != string.Empty)
+                DialogManager.Instance.OpenDefaultDialog(dialogContents, DialogType.FAIL);
         }
-        else
-            dialogContents = "문장을 조합 할 수 없습니다\n단어를 확인해주세요.";
-
-        if (dialogContents != string.Empty)
-            DialogManager.Instance.OpenDefaultDialog(dialogContents, DialogType.FAIL);
     }
-
     public void Activate(GameObject target, GameObject indicator) {
         baseFrame.Activate(target, indicator);
     }
