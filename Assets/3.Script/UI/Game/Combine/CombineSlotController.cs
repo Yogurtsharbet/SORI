0d99bf2f6fa8aeb80b2f62b66eb25f9ed36630bc
김수진 <nuu042@naver.com>
2024-09-19 20:39:47 +0900

    [Update] add rectTransform

--- Assets/3.Script/UI/Game/Combine/CombineSlotController.cs
+++ Assets/3.Script/UI/Game/Combine/CombineSlotController.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // [UI] 조합 - 실제 조합 slot
 public class CombineSlotController : MonoBehaviour {
+    private RectTransform rectTransform;
 
+    private void Awake() {
+        rectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    
 }
