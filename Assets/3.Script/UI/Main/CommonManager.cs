b34b481ecf0dbd25d35fce60df5198cd5d3a48bd
전홍현 <toinbee3@naver.com>
2024-10-09 18:27:28 +0900

    [Fix] SFX 수정

--- Assets/3.Script/UI/Main/CommonManager.cs
+++ Assets/3.Script/UI/Main/CommonManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CommonManager : MonoBehaviour {
     private static CommonManager instance = null;
+    [SerializeField] private Material daySkybox;
 
     public static CommonManager Instance {
         get {
@@ -20,5 +21,12 @@ public class CommonManager : MonoBehaviour {
         else {
             Destroy(gameObject);
         }
+    }
+
+    private void Start() {
+        var directionalLight = GetComponentInChildren<Light>();
+        directionalLight.color = new Color(1f, 0.956f, 0.839f);
+        RenderSettings.ambientLight = new Color(0.90f, 0.76f, 0.63f);
+        RenderSettings.skybox = daySkybox;
     }
 }
